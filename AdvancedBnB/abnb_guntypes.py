511a34a8c3f55f73808332f30b890df7f7dfab48
andressmithuis <andressmithuis@yahoo.com>
2025-07-07 19:02:19 +0200

    [ABnB Guns]
    Added weapon base stats for tiers 2+

--- AdvancedBnB/abnb_guntypes.py
+++ AdvancedBnB/abnb_guntypes.py
@@ -19,7 +19,16 @@ class Pistol(Guntype):
     @staticmethod
     def get_basestats(tier):
         table = {
-            1: {'hit_dice': Dice.from_string('1d6'), 'crit_dice': Dice.from_string('1d8'), 'hits_crits' : {'glance': {'hits': 1, 'crits': 0}, 'solid': {'hits': 2, 'crits': 0}, 'penetrate': {'hits': 3, 'crits': 1}}, 'range': 5, 'mag_size': 3},
+            1: {'hit_dice': Dice.from_string('1d6'), 'crit_dice': Dice.from_string('1d8'), 'hits_crits': {'glance': {'hits': 1, 'crits': 0}, 'solid': {'hits': 2, 'crits': 0}, 'penetrate': {'hits': 3, 'crits': 1}}, 'range': 5, 'mag_size': 3},
+            2: {'hit_dice': Dice.from_string('1d6'), 'crit_dice': Dice.from_string('1d8'), 'hits_crits': {'glance': {'hits': 2, 'crits': 0}, 'solid': {'hits': 2, 'crits': 0}, 'penetrate': {'hits': 4, 'crits': 1}}, 'range': 5, 'mag_size': 3},
+            3: {'hit_dice': Dice.from_string('1d6'), 'crit_dice': Dice.from_string('1d8'), 'hits_crits': {'glance': {'hits': 2, 'crits': 0}, 'solid': {'hits': 2, 'crits': 1}, 'penetrate': {'hits': 3, 'crits': 2}}, 'range': 5, 'mag_size': 3},
+            4: {'hit_dice': Dice.from_string('2d6'), 'crit_dice': Dice.from_string('2d8'), 'hits_crits': {'glance': {'hits': 1, 'crits': 0}, 'solid': {'hits': 1, 'crits': 1}, 'penetrate': {'hits': 2, 'crits': 1}}, 'range': 5, 'mag_size': 3},
+            5: {'hit_dice': Dice.from_string('2d6'), 'crit_dice': Dice.from_string('2d8'), 'hits_crits': {'glance': {'hits': 1, 'crits': 0}, 'solid': {'hits': 2, 'crits': 1}, 'penetrate': {'hits': 3, 'crits': 1}}, 'range': 5, 'mag_size': 3},
+            6: {'hit_dice': Dice.from_string('2d6'), 'crit_dice': Dice.from_string('2d8'), 'hits_crits': {'glance': {'hits': 2, 'crits': 0}, 'solid': {'hits': 2, 'crits': 1}, 'penetrate': {'hits': 2, 'crits': 2}}, 'range': 5, 'mag_size': 3},
+            7: {'hit_dice': Dice.from_string('2d6'), 'crit_dice': Dice.from_string('2d8'), 'hits_crits': {'glance': {'hits': 2, 'crits': 0}, 'solid': {'hits': 3, 'crits': 1}, 'penetrate': {'hits': 3, 'crits': 2}}, 'range': 5, 'mag_size': 3},
+            8: {'hit_dice': Dice.from_string('3d6'), 'crit_dice': Dice.from_string('3d8'), 'hits_crits': {'glance': {'hits': 2, 'crits': 0}, 'solid': {'hits': 2, 'crits': 1}, 'penetrate': {'hits': 3, 'crits': 1}}, 'range': 5, 'mag_size': 3},
+            9: {'hit_dice': Dice.from_string('3d6'), 'crit_dice': Dice.from_string('3d8'), 'hits_crits': {'glance': {'hits': 2, 'crits': 0}, 'solid': {'hits': 2, 'crits': 1}, 'penetrate': {'hits': 3, 'crits': 2}}, 'range': 5, 'mag_size': 3},
+            10: {'hit_dice': Dice.from_string('3d6'), 'crit_dice': Dice.from_string('3d8'), 'hits_crits': {'glance': {'hits': 3, 'crits': 0}, 'solid': {'hits': 2, 'crits': 1}, 'penetrate': {'hits': 2, 'crits': 3}}, 'range': 5, 'mag_size': 3},
         }
 
         return table[tier]
@@ -32,7 +41,16 @@ class Smg(Guntype):
     @staticmethod
     def get_basestats(tier):
         table = {
-            1: {'hit_dice': Dice.from_string('2d4'), 'crit_dice': Dice.from_string('2d6'), 'hits_crits' : {'glance': {'hits': 1, 'crits': 0}, 'solid': {'hits': 2, 'crits': 0}, 'penetrate': {'hits': 1, 'crits': 1}}, 'range': 5, 'mag_size': 4},
+            1: {'hit_dice': Dice.from_string('2d4'), 'crit_dice': Dice.from_string('2d6'), 'hits_crits': {'glance': {'hits': 1, 'crits': 0}, 'solid': {'hits': 2, 'crits': 0}, 'penetrate': {'hits': 1, 'crits': 1}}, 'range': 5, 'mag_size': 4},
+            2: {'hit_dice': Dice.from_string('2d4'), 'crit_dice': Dice.from_string('2d6'), 'hits_crits': {'glance': {'hits': 2, 'crits': 0}, 'solid': {'hits': 3, 'crits': 0}, 'penetrate': {'hits': 2, 'crits': 1}}, 'range': 5, 'mag_size': 4},
+            3: {'hit_dice': Dice.from_string('2d4'), 'crit_dice': Dice.from_string('2d6'), 'hits_crits': {'glance': {'hits': 3, 'crits': 0}, 'solid': {'hits': 4, 'crits': 0}, 'penetrate': {'hits': 3, 'crits': 1}}, 'range': 5, 'mag_size': 4},
+            4: {'hit_dice': Dice.from_string('3d4'), 'crit_dice': Dice.from_string('3d6'), 'hits_crits': {'glance': {'hits': 2, 'crits': 0}, 'solid': {'hits': 3, 'crits': 0}, 'penetrate': {'hits': 3, 'crits': 1}}, 'range': 5, 'mag_size': 4},
+            5: {'hit_dice': Dice.from_string('3d4'), 'crit_dice': Dice.from_string('3d6'), 'hits_crits': {'glance': {'hits': 2, 'crits': 0}, 'solid': {'hits': 4, 'crits': 0}, 'penetrate': {'hits': 3, 'crits': 1}}, 'range': 5, 'mag_size': 4},
+            6: {'hit_dice': Dice.from_string('3d4'), 'crit_dice': Dice.from_string('3d6'), 'hits_crits': {'glance': {'hits': 3, 'crits': 0}, 'solid': {'hits': 4, 'crits': 0}, 'penetrate': {'hits': 3, 'crits': 1}}, 'range': 5, 'mag_size': 4},
+            7: {'hit_dice': Dice.from_string('3d4'), 'crit_dice': Dice.from_string('3d6'), 'hits_crits': {'glance': {'hits': 3, 'crits': 0}, 'solid': {'hits': 3, 'crits': 1}, 'penetrate': {'hits': 3, 'crits': 1}}, 'range': 5, 'mag_size': 4},
+            8: {'hit_dice': Dice.from_string('4d4'), 'crit_dice': Dice.from_string('4d6'), 'hits_crits': {'glance': {'hits': 3, 'crits': 0}, 'solid': {'hits': 3, 'crits': 0}, 'penetrate': {'hits': 3, 'crits': 1}}, 'range': 5, 'mag_size': 4},
+            9: {'hit_dice': Dice.from_string('4d4'), 'crit_dice': Dice.from_string('4d6'), 'hits_crits': {'glance': {'hits': 3, 'crits': 0}, 'solid': {'hits': 2, 'crits': 1}, 'penetrate': {'hits': 3, 'crits': 2}}, 'range': 5, 'mag_size': 4},
+            10: {'hit_dice': Dice.from_string('4d4'), 'crit_dice': Dice.from_string('4d6'), 'hits_crits': {'glance': {'hits': 4, 'crits': 0}, 'solid': {'hits': 2, 'crits': 1}, 'penetrate': {'hits': 3, 'crits': 2}}, 'range': 5, 'mag_size': 4},
         }
 
         return table[tier]
@@ -45,7 +63,16 @@ class Rifle(Guntype):
     @staticmethod
     def get_basestats(tier):
         table = {
-            1: {'hit_dice': Dice.from_string('1d8'), 'crit_dice': Dice.from_string('1d10'), 'hits_crits' : {'glance': {'hits': 1, 'crits': 0}, 'solid': {'hits': 3, 'crits': 0}, 'penetrate': {'hits': 3, 'crits': 1}}, 'range': 6, 'mag_size': 5},
+            1: {'hit_dice': Dice.from_string('1d8'), 'crit_dice': Dice.from_string('1d10'), 'hits_crits': {'glance': {'hits': 1, 'crits': 0}, 'solid': {'hits': 3, 'crits': 0}, 'penetrate': {'hits': 3, 'crits': 1}}, 'range': 6, 'mag_size': 5},
+            2: {'hit_dice': Dice.from_string('1d8'), 'crit_dice': Dice.from_string('1d10'), 'hits_crits': {'glance': {'hits': 2, 'crits': 0}, 'solid': {'hits': 3, 'crits': 0}, 'penetrate': {'hits': 3, 'crits': 1}}, 'range': 6, 'mag_size': 5},
+            3: {'hit_dice': Dice.from_string('1d8'), 'crit_dice': Dice.from_string('1d10'), 'hits_crits': {'glance': {'hits': 1, 'crits': 1}, 'solid': {'hits': 3, 'crits': 1}, 'penetrate': {'hits': 2, 'crits': 2}}, 'range': 6, 'mag_size': 5},
+            4: {'hit_dice': Dice.from_string('1d8'), 'crit_dice': Dice.from_string('1d10'), 'hits_crits': {'glance': {'hits': 2, 'crits': 1}, 'solid': {'hits': 3, 'crits': 1}, 'penetrate': {'hits': 3, 'crits': 2}}, 'range': 6, 'mag_size': 5},
+            5: {'hit_dice': Dice.from_string('1d8'), 'crit_dice': Dice.from_string('1d10'), 'hits_crits': {'glance': {'hits': 2, 'crits': 1}, 'solid': {'hits': 3, 'crits': 1}, 'penetrate': {'hits': 3, 'crits': 2}}, 'range': 6, 'mag_size': 5},
+            6: {'hit_dice': Dice.from_string('2d8'), 'crit_dice': Dice.from_string('2d10'), 'hits_crits': {'glance': {'hits': 1, 'crits': 0}, 'solid': {'hits': 2, 'crits': 1}, 'penetrate': {'hits': 2, 'crits': 1}}, 'range': 6, 'mag_size': 5},
+            7: {'hit_dice': Dice.from_string('2d8'), 'crit_dice': Dice.from_string('2d10'), 'hits_crits': {'glance': {'hits': 1, 'crits': 1}, 'solid': {'hits': 2, 'crits': 1}, 'penetrate': {'hits': 3, 'crits': 1}}, 'range': 6, 'mag_size': 5},
+            8: {'hit_dice': Dice.from_string('2d8'), 'crit_dice': Dice.from_string('2d10'), 'hits_crits': {'glance': {'hits': 1, 'crits': 1}, 'solid': {'hits': 3, 'crits': 1}, 'penetrate': {'hits': 2, 'crits': 2}}, 'range': 6, 'mag_size': 5},
+            9: {'hit_dice': Dice.from_string('2d8'), 'crit_dice': Dice.from_string('2d10'), 'hits_crits': {'glance': {'hits': 2, 'crits': 1}, 'solid': {'hits': 3, 'crits': 1}, 'penetrate': {'hits': 3, 'crits': 2}}, 'range': 6, 'mag_size': 5},
+            10: {'hit_dice': Dice.from_string('2d8'), 'crit_dice': Dice.from_string('2d10'), 'hits_crits': {'glance': {'hits': 2, 'crits': 1}, 'solid': {'hits': 3, 'crits': 1}, 'penetrate': {'hits': 3, 'crits': 3}}, 'range': 6, 'mag_size': 5},
         }
 
         return table[tier]
@@ -64,7 +91,16 @@ class Shotgun(Guntype):
     @staticmethod
     def get_basestats(tier):
         table = {
-            1: {'hit_dice': Dice.from_string('1d8'), 'crit_dice': Dice.from_string('1d10'), 'hits_crits' : {'glance': {'hits': 1, 'crits': 0}, 'solid': {'hits': 2, 'crits': 0}, 'penetrate': {'hits': 1, 'crits': 1}}, 'range': 4, 'mag_size': 3},
+            1: {'hit_dice': Dice.from_string('1d8'), 'crit_dice': Dice.from_string('1d10'), 'hits_crits': {'glance': {'hits': 1, 'crits': 0}, 'solid': {'hits': 2, 'crits': 0}, 'penetrate': {'hits': 1, 'crits': 1}}, 'range': 4, 'mag_size': 3},
+            2: {'hit_dice': Dice.from_string('1d8'), 'crit_dice': Dice.from_string('1d10'), 'hits_crits': {'glance': {'hits': 1, 'crits': 0}, 'solid': {'hits': 3, 'crits': 0}, 'penetrate': {'hits': 3, 'crits': 1}}, 'range': 4, 'mag_size': 3},
+            3: {'hit_dice': Dice.from_string('1d8'), 'crit_dice': Dice.from_string('1d10'), 'hits_crits': {'glance': {'hits': 2, 'crits': 0}, 'solid': {'hits': 2, 'crits': 1}, 'penetrate': {'hits': 4, 'crits': 1}}, 'range': 4, 'mag_size': 3},
+            4: {'hit_dice': Dice.from_string('1d8'), 'crit_dice': Dice.from_string('1d10'), 'hits_crits': {'glance': {'hits': 3, 'crits': 0}, 'solid': {'hits': 3, 'crits': 1}, 'penetrate': {'hits': 4, 'crits': 1}}, 'range': 4, 'mag_size': 3},
+            5: {'hit_dice': Dice.from_string('1d8'), 'crit_dice': Dice.from_string('1d10'), 'hits_crits': {'glance': {'hits': 3, 'crits': 0}, 'solid': {'hits': 3, 'crits': 1}, 'penetrate': {'hits': 4, 'crits': 2}}, 'range': 4, 'mag_size': 3},
+            6: {'hit_dice': Dice.from_string('1d8'), 'crit_dice': Dice.from_string('1d10'), 'hits_crits': {'glance': {'hits': 3, 'crits': 0}, 'solid': {'hits': 4, 'crits': 1}, 'penetrate': {'hits': 4, 'crits': 2}}, 'range': 4, 'mag_size': 3},
+            7: {'hit_dice': Dice.from_string('2d8'), 'crit_dice': Dice.from_string('2d10'), 'hits_crits': {'glance': {'hits': 2, 'crits': 0}, 'solid': {'hits': 2, 'crits': 0}, 'penetrate': {'hits': 3, 'crits': 1}}, 'range': 4, 'mag_size': 3},
+            8: {'hit_dice': Dice.from_string('2d8'), 'crit_dice': Dice.from_string('2d10'), 'hits_crits': {'glance': {'hits': 2, 'crits': 0}, 'solid': {'hits': 2, 'crits': 0}, 'penetrate': {'hits': 3, 'crits': 2}}, 'range': 4, 'mag_size': 3},
+            9: {'hit_dice': Dice.from_string('2d8'), 'crit_dice': Dice.from_string('2d10'), 'hits_crits': {'glance': {'hits': 2, 'crits': 0}, 'solid': {'hits': 3, 'crits': 0}, 'penetrate': {'hits': 3, 'crits': 2}}, 'range': 4, 'mag_size': 3},
+            10: {'hit_dice': Dice.from_string('2d8'), 'crit_dice': Dice.from_string('2d10'), 'hits_crits': {'glance': {'hits': 2, 'crits': 0}, 'solid': {'hits': 2, 'crits': 1}, 'penetrate': {'hits': 3, 'crits': 2}}, 'range': 4, 'mag_size': 3},
         }
 
         return table[tier]
@@ -85,7 +121,16 @@ class Sniper(Guntype):
     @staticmethod
     def get_basestats(tier):
         table = {
-            1: {'hit_dice': Dice.from_string('1d12'), 'crit_dice': Dice.from_string('1d20'), 'hits_crits' : {'glance': {'hits': 0, 'crits': 0}, 'solid': {'hits': 1, 'crits': 0}, 'penetrate': {'hits': 0, 'crits': 1}}, 'range': 8, 'mag_size': 2},
+            1: {'hit_dice': Dice.from_string('1d12'), 'crit_dice': Dice.from_string('1d20'), 'hits_crits': {'glance': {'hits': 0, 'crits': 0}, 'solid': {'hits': 1, 'crits': 0}, 'penetrate': {'hits': 0, 'crits': 1}}, 'range': 8, 'mag_size': 2},
+            2: {'hit_dice': Dice.from_string('1d12'), 'crit_dice': Dice.from_string('1d20'), 'hits_crits': {'glance': {'hits': 0, 'crits': 0}, 'solid': {'hits': 1, 'crits': 0}, 'penetrate': {'hits': 1, 'crits': 1}}, 'range': 8, 'mag_size': 2},
+            3: {'hit_dice': Dice.from_string('1d12'), 'crit_dice': Dice.from_string('1d20'), 'hits_crits': {'glance': {'hits': 0, 'crits': 0}, 'solid': {'hits': 1, 'crits': 0}, 'penetrate': {'hits': 0, 'crits': 2}}, 'range': 8, 'mag_size': 2},
+            4: {'hit_dice': Dice.from_string('1d12'), 'crit_dice': Dice.from_string('1d20'), 'hits_crits': {'glance': {'hits': 0, 'crits': 0}, 'solid': {'hits': 1, 'crits': 0}, 'penetrate': {'hits': 1, 'crits': 2}}, 'range': 8, 'mag_size': 2},
+            5: {'hit_dice': Dice.from_string('2d12'), 'crit_dice': Dice.from_string('2d20'), 'hits_crits': {'glance': {'hits': 0, 'crits': 0}, 'solid': {'hits': 1, 'crits': 0}, 'penetrate': {'hits': 1, 'crits': 1}}, 'range': 8, 'mag_size': 2},
+            6: {'hit_dice': Dice.from_string('2d12'), 'crit_dice': Dice.from_string('2d20'), 'hits_crits': {'glance': {'hits': 0, 'crits': 0}, 'solid': {'hits': 1, 'crits': 0}, 'penetrate': {'hits': 0, 'crits': 2}}, 'range': 8, 'mag_size': 2},
+            7: {'hit_dice': Dice.from_string('2d12'), 'crit_dice': Dice.from_string('2d20'), 'hits_crits': {'glance': {'hits': 0, 'crits': 0}, 'solid': {'hits': 1, 'crits': 0}, 'penetrate': {'hits': 1, 'crits': 2}}, 'range': 8, 'mag_size': 2},
+            8: {'hit_dice': Dice.from_string('2d12'), 'crit_dice': Dice.from_string('2d20'), 'hits_crits': {'glance': {'hits': 0, 'crits': 0}, 'solid': {'hits': 2, 'crits': 0}, 'penetrate': {'hits': 1, 'crits': 2}}, 'range': 8, 'mag_size': 2},
+            9: {'hit_dice': Dice.from_string('2d12'), 'crit_dice': Dice.from_string('2d20'), 'hits_crits': {'glance': {'hits': 0, 'crits': 0}, 'solid': {'hits': 2, 'crits': 0}, 'penetrate': {'hits': 2, 'crits': 2}}, 'range': 8, 'mag_size': 2},
+            10: {'hit_dice': Dice.from_string('2d12'), 'crit_dice': Dice.from_string('2d20'), 'hits_crits': {'glance': {'hits': 0, 'crits': 0}, 'solid': {'hits': 2, 'crits': 0}, 'penetrate': {'hits': 1, 'crits': 3}}, 'range': 8, 'mag_size': 2},
         }
 
         return table[tier]
@@ -99,6 +144,15 @@ class Launcher(Guntype):
     def get_basestats(tier):
         table = {
             1: {'hit_dice': Dice.from_string('1d10'), 'crit_dice': Dice.from_string('1d12'), 'hits_crits' : {'glance': {'hits': 1, 'crits': 0}, 'solid': {'hits': 1, 'crits': 0}, 'penetrate': {'hits': 1, 'crits': 1}}, 'range': 4, 'mag_size': 1},
+            2: {'hit_dice': Dice.from_string('1d10'), 'crit_dice': Dice.from_string('1d12'), 'hits_crits': {'glance': {'hits': 1, 'crits': 0}, 'solid': {'hits': 2, 'crits': 0}, 'penetrate': {'hits': 2, 'crits': 1}}, 'range': 4, 'mag_size': 1},
+            3: {'hit_dice': Dice.from_string('1d10'), 'crit_dice': Dice.from_string('1d12'), 'hits_crits': {'glance': {'hits': 1, 'crits': 0}, 'solid': {'hits': 2, 'crits': 0}, 'penetrate': {'hits': 3, 'crits': 1}}, 'range': 4, 'mag_size': 1},
+            4: {'hit_dice': Dice.from_string('2d10'), 'crit_dice': Dice.from_string('2d12'), 'hits_crits': {'glance': {'hits': 1, 'crits': 0}, 'solid': {'hits': 1, 'crits': 0}, 'penetrate': {'hits': 1, 'crits': 1}}, 'range': 4, 'mag_size': 1},
+            5: {'hit_dice': Dice.from_string('2d10'), 'crit_dice': Dice.from_string('2d12'), 'hits_crits': {'glance': {'hits': 1, 'crits': 0}, 'solid': {'hits': 1, 'crits': 0}, 'penetrate': {'hits': 2, 'crits': 1}}, 'range': 4, 'mag_size': 1},
+            6: {'hit_dice': Dice.from_string('2d10'), 'crit_dice': Dice.from_string('2d12'), 'hits_crits': {'glance': {'hits': 1, 'crits': 0}, 'solid': {'hits': 2, 'crits': 0}, 'penetrate': {'hits': 2, 'crits': 1}}, 'range': 4, 'mag_size': 1},
+            7: {'hit_dice': Dice.from_string('2d10'), 'crit_dice': Dice.from_string('2d12'), 'hits_crits': {'glance': {'hits': 2, 'crits': 0}, 'solid': {'hits': 2, 'crits': 0}, 'penetrate': {'hits': 2, 'crits': 1}}, 'range': 4, 'mag_size': 1},
+            8: {'hit_dice': Dice.from_string('2d10'), 'crit_dice': Dice.from_string('2d12'), 'hits_crits': {'glance': {'hits': 2, 'crits': 0}, 'solid': {'hits': 3, 'crits': 0}, 'penetrate': {'hits': 2, 'crits': 1}}, 'range': 4, 'mag_size': 1},
+            9: {'hit_dice': Dice.from_string('2d10'), 'crit_dice': Dice.from_string('2d12'), 'hits_crits': {'glance': {'hits': 2, 'crits': 0}, 'solid': {'hits': 3, 'crits': 0}, 'penetrate': {'hits': 3, 'crits': 1}}, 'range': 4, 'mag_size': 1},
+            10: {'hit_dice': Dice.from_string('3d10'), 'crit_dice': Dice.from_string('3d12'), 'hits_crits': {'glance': {'hits': 2, 'crits': 0}, 'solid': {'hits': 2, 'crits': 0}, 'penetrate': {'hits': 2, 'crits': 1}}, 'range': 4, 'mag_size': 1},
         }
 
         return table[tier]
